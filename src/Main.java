143947695705f89c7f950610e3f62b2983cfcb03
Tatiana080811 <Kazantseva2pochta@gmail.com>
2025-06-14 18:27:40 +0700

    HW8

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
         int[] weights = {1, 2, 3};
         for (int i = 0; i < 3; i++) {
             System.out.print(weights[i]);
-            if (i != weights.length -1) {
+            if (i != weights.length - 1) {
                 System.out.print(", ");
             }
         }
@@ -20,48 +21,55 @@ public class Main {
         }
         System.out.println();
         int[] speed = {60, 80, 120, 200, 250};
-        for (int i1 = 0; i1 < 5; i1++) {
-                System.out.print(speed[i1]);
-                if (i1 != speed.length - 1);
-            System.out.print(", ");
+        for (int i1 = 0; i1 < speed.length; i1++) {
+            System.out.print(speed[i1]);
+            if (i1 < speed.length - 1) {
+                System.out.print(", ");
             }
+        }
 
-            System.out.println();
-            weights = new int[]{1, 2, 3};
-            for (int i = weights.length - 1; i >= 0; i--) {
-                System.out.print(weights[i]);
-                if (i != 0) {
-                    System.out.print(", ");
-                }
 
-            }
         System.out.println();
-            height = new double[]{1.57, 7.654, 9.986};
-            for (int d = height.length -1; d >= 0; d--) {
-                System.out.print(height[d]);
-                if (d != 0) {
-                    System.out.print(", ");
-                }
-            }
-        System.out.println();
-            speed = new int[]{60, 80, 120, 200, 250};
-            for (int i1 = speed.length - 1; i1 >= 0; i1--) {
-                System.out.print(speed[i1]);
-                if (i1 != 0) {
-                    System.out.print(", ");
-                }
-            }
-        System.out.println();
+
         weights = new int[]{1, 2, 3};
-        for (int i = 0; i < weights.length; i++) {
-            if (weights [i] % 2 == 1) {
-                weights[i] += 1;
+        for (int i = weights.length - 1; i >= 0; i--) {
+            System.out.print(weights[i]);
+            if (i != 0) {
+                System.out.print(", ");
             }
-                System.out.println(weights[1]);
-            }
-        }
 
         }
+        System.out.println();
+        height = new double[]{1.57, 7.654, 9.986};
+        for (int d = height.length - 1; d >= 0; d--) {
+            System.out.print(height[d]);
+            if (d != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        speed = new int[]{60, 80, 120, 200, 250};
+        for (int i1 = speed.length - 1; i1 >= 0; i1--) {
+            System.out.print(speed[i1]);
+            if (i1 != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        int[] array = {1, 2, 3};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i]++;
+            }
+
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+
+
+
+
 
 
 
